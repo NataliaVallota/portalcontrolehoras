086bd03e1df673154319861cc74c23e23d19529e
Thiago Henrique Mira Muche <987301748@TREDOTIWX080-VM.itaud.des.ihf>
2020-09-18 18:47:55 -0300

    Comite final do dia

---
+++ src/main/java/br/com/itau/portalcontrolehoras/controller/AtividadesController.java
@@ -0,0 +1,28 @@
+package br.com.itau.portalcontrolehoras.controller;
+
+import java.util.ArrayList;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.itau.portalcontrolehoras.DAO.AtividadeDAO;
+import br.com.itau.portalcontrolehoras.beans.Atividade;
+
+@RestController
+public class AtividadesController {
+	
+	@Autowired
+	private AtividadeDAO dao;
+	
+	@GetMapping("/Atividades")
+	public ResponseEntity<ArrayList<Atividade>> getAtividades(){
+		ArrayList<Atividade> resposta = (ArrayList<Atividade>)dao.findAll();
+		if (resposta.size() == 0) {
+			return ResponseEntity.status(404).build();			
+		}
+		return ResponseEntity.ok(resposta);
+	}
+
+}
---
+++ src/main/java/br/com/itau/portalcontrolehoras/controller/OcorrenciasController.java
@@ -0,0 +1,66 @@
+package br.com.itau.portalcontrolehoras.controller;
+
+import java.util.ArrayList;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.itau.portalcontrolehoras.DAO.OcorrenciaDAO;
+import br.com.itau.portalcontrolehoras.DAO.UsuarioDAO;
+import br.com.itau.portalcontrolehoras.beans.Ocorrencia;
+import br.com.itau.portalcontrolehoras.beans.Usuario;
+
+@RestController
+public class OcorrenciasController {
+	
+	@Autowired
+	private OcorrenciaDAO dao;
+	
+	@Autowired
+	private UsuarioDAO daoUser;
+	
+	@GetMapping("/Ocorrencias")
+	public ResponseEntity<ArrayList<Ocorrencia>> getOcorrencias(){
+		ArrayList<Ocorrencia> resposta = (ArrayList<Ocorrencia>)dao.findAll();
+		if (resposta.size() == 0) {
+			return ResponseEntity.status(404).build();			
+		}
+		return ResponseEntity.ok(resposta);
+	}
+	
+	@GetMapping("/ListaOcorrenciasID/{id}")
+	public ResponseEntity<ArrayList<Ocorrencia>> getOcorrenciasPorUsuarioId(@PathVariable int id){
+		Usuario usuario = new Usuario();
+		usuario.setId_usuario(id);
+		ArrayList<Ocorrencia> resposta = (ArrayList<Ocorrencia>)dao.findByUsuario(usuario);
+		if (resposta.size() == 0) {
+			return ResponseEntity.status(404).build();			
+		}
+		return ResponseEntity.ok(resposta);
+	}
+	
+	@GetMapping("/ListaOcorrenciasRacf/{racf}")
+	public ResponseEntity<ArrayList<Ocorrencia>> getOcorrenciasPorUsuarioRacf(@PathVariable String racf){		
+		Usuario usuario = daoUser.findByRacf(racf);
+		ArrayList<Ocorrencia> resposta = (ArrayList<Ocorrencia>)dao.findByUsuario(usuario);
+		if (resposta.size() == 0) {
+			return ResponseEntity.status(404).build();			
+		}
+		return ResponseEntity.ok(resposta);
+	}
+	
+	@PostMapping("/AtualizaOcorrencia")
+	public ResponseEntity<Ocorrencia> doAtualizaOcorrencia(@RequestBody Ocorrencia ocorrencia){
+		Ocorrencia resposta = dao.save(ocorrencia);
+		if (resposta==null) {
+			return ResponseEntity.status(404).build();			
+		}
+		return ResponseEntity.ok(resposta);
+	}
+
+}
